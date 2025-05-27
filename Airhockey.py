4bac8f904ff32eb2c92b6c644c27cb6631417874
c0a2415032 <c0a2415032@edu.teu.ac.jp>
2025-05-27 13:38:38 +0900

    パックとスマッシャー

--- Airhockey.py
+++ Airhockey.py
@@ -7,7 +7,6 @@ import pygame as pg
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
-NUM_OF_BOMBS = 1
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
@@ -22,6 +21,36 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
+
+class Puck:
+    """
+    puckに関するクラス
+    """
+    def __init__(self, color: tuple[int, int, int], rad: int):
+        """
+        puck
+        """
+        self.img = pg.Surface((2*rad, 2*rad))
+        pg.draw.circle(self.img, color, (rad, rad), rad)
+        self.img.set_colorkey((0, 0, 0))
+        self.rct = self.img.get_rect()
+        self.rct.center = 550, 325
+        self.vx, self.vy = +1, +1
+
+    def update(self, screen: pg.Surface):
+        """
+        puckを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        yoko, tate = check_bound(self.rct)
+        if not yoko:
+            self.vx *= -1
+        if not tate:
+            self.vy *= -1
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)
+        # pg.display.update()
+
 
 
 class Smasher1:
@@ -53,6 +82,7 @@ class Smasher1:
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(self.img, self.rct)
+        # pg.display.update()
 
 class Smasher2:
     """
@@ -83,13 +113,16 @@ class Smasher2:
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(self.img, self.rct)
+        # pg.display.update()
 
 def main():
     pg.display.set_caption("Air hockey")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    screen.blit(bg_img, [0, 0])
     smasher1 = Smasher1([300, 200])
     smasher2 = Smasher2([800, 200])
+    puck = Puck((105, 105, 105), 35)
 
     while True:
         screen.blit(bg_img, [0, 0])
@@ -100,44 +133,28 @@ def main():
         key_lst = pg.key.get_pressed()
         smasher1.update(key_lst, screen)
         smasher2.update(key_lst, screen)
-        pg.display.update()
-    COUNTER = 0 #スコアを格納する変数
-    pg.display.set_caption("たたかえ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))    
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    bird = Bird((300, 200))
-    bombs = [Bomb((105, 105, 105), 35) for _ in range(NUM_OF_BOMBS)]
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
-        screen.blit(bg_img, [0, 0])
-        
-        # if bomb is not None:
-        for bomb in bombs:
-             if smasher1.rct.colliderect(bomb.rct):
-                 # こうかとんに当たったらバウンド
-                 bomb.vx *= -1
-                 bomb.vy *= -1
-        for bomb in bombs:
-             if smasher2.rct.colliderect(bomb.rct):
-                 # こうかとんに当たったらバウンド
-                 bomb.vx *= -1
-                 bomb.vy *= -1
+        puck.update(screen)
 
-        
-        
+        COUNTER = 0 #スコアを格納する変数
+        if smasher1.rct.colliderect(puck.rct):
+            # こうかとんに当たったらバウンド
+            puck.vx *= -1
+            puck.vy *= -1
+        if smasher2.rct.colliderect(puck.rct):
+            # こうかとんに当たったらバウンド
+            puck.vx *= -1
+            puck.vy *= -1
 
-        key_lst = pg.key.get_pressed()
-        bird.update(key_lst, screen) 
-        #Scoreクラスのインスタンスを作成し、updateメソッドでblit
-        score = Score(COUNTER)
-        score.update(screen)
-        for bomb in bombs:
-            bomb.update(screen)
+            
+            
+
+
+            #Scoreクラスのインスタンスを作成し、updateメソッドでblit
+            # score = Score(COUNTER)
+            # score.update(screen)
         pg.display.update()
-        # tmr += 1
-        # clock.tick(200)
+            # tmr += 1
+            # clock.tick(200)
 
 
 if __name__ == "__main__":
